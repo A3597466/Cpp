8b3f463bb742d8c2fd90c2e4e1d97c1b4d25afe1
wosunny2008 <wosunny@126.com>
2020-12-18 09:15:04 +0800

    1st review

---
+++ max.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin >>a>>b;
+    c=max(a,b);
+    cout <<"max is "<<c<<endl;
+}
+int max(int x, int y){return x>y?x:y;}
